f7a5ee4d9f7a0370a01b407067ef8c0bf247fa86
nevo <xhua@vmware.com>
2011-03-01 18:23:49 +0800

    Random test with libjpeg for ffi.

--- src/xcode/iRacket/Classes/test.rkt
+++ src/xcode/iRacket/Classes/test.rkt
@@ -8,6 +8,8 @@
 (iloveracket)
 (printf "~n")
 
+(system-type)
+
 ; test path/directory
 (getenv "PATH")
 (current-directory)
@@ -30,3 +32,27 @@
 (define _png_end_infop (_cpointer 'png_end_infop))
 (define _png_size_t _long)
 (define-png png_access_version_number (_fun -> _uint32))
+
+;; libjpeg
+(define-ffi-definer define-jpeg main-lib
+  #:provide provide)
+(define _j_common_ptr _pointer)
+(define _size_t _long)
+(define _JDIMENSION _uint)
+(define _J_COLOR_SPACE _int)
+(define _J_DCT_METHOD _int)
+(define _J_DITHER_MODE _int)
+(define _pool_id _int)
+(define JPOOL_PERMANENT 0)
+(define JPOOL_IMAGE 1)
+(define JPOOL_NUMPOOLS 2)
+(define JMSG_LENGTH_MAX  200)
+
+(define-cstruct _jpeg_error_mgr ([error_exit (_fun _j_common_ptr -> _void)]
+                                 [emit_message _pointer]
+                                 [output_message _pointer]
+                                 [format_message (_fun _j_common_ptr _pointer -> _void)]
+                                 ;; and more, including an inline character                                                                       
+                                 ;; array that is a pain to handle here                                                                           
+                                 ))
+(define sizeof_jpeg_error_mgr 1024)
